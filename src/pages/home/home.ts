4d34ab3f0b8b137006f16d42df96a54061133abe
Marcos Guedes <marcosguedes01@gmail.com>
2017-09-12 21:06:46 -0300

    exemplo de propaganda admob

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public admob: AdMobFree) {
 
+  }
+
+  showBanner() {
+    let bannerConfig: AdMobFreeBannerConfig = {
+      isTesting: true, // Remove in production
+      autoShow: true,
+      //id: Your Ad Unit ID goes here
+    };
+    
+    this.admob.banner.config(bannerConfig);
+    
+    this.admob.banner.prepare().then(() => {
+      // success
+    }).catch(e => console.log(e));
+  }
+
+  launchInterstitial() {
+    let interstitialConfig: AdMobFreeInterstitialConfig = {
+      isTesting: true, // Remove in production
+      autoShow: true
+      //id: Your Ad Unit ID goes here
+    }; 
+    
+    this.admob.interstitial.config(interstitialConfig);
+    
+    this.admob.interstitial.prepare().then(() => {
+      // success
+    });
   }
 
 }
